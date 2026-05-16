0763cbd9a03a130ddb3ac4486b83d8510cd3636d
rwkt21 <rwkt21@bath.ac.uk>
2026-05-16 03:48:18 +0000

    update

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ conn.execute("DROP TABLE IF EXISTS pet")
 conn.execute("CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups SMALLINT UNSIGNED, birth DATE, death DATE)")
 
 print ("Table created successfully")
+
+"INSERT INTO pet VALUES (1, 'Fluffy', 'Alice', 'cat', 'f', 5, '2001-02-04', null)"
