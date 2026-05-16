910c819fb9a414bb1b74c698c86909a315a5846a
rwkt21 <rwkt21@bath.ac.uk>
2026-05-16 03:47:21 +0000

    update

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+import sqlite3
+conn = sqlite3.connect('store')
+print ("Database has been created")
+
+conn.execute("DROP TABLE IF EXISTS pet")
+
+conn.execute("CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups SMALLINT UNSIGNED, birth DATE, death DATE)")
+
+print ("Table created successfully")
