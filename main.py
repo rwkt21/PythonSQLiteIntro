b24dfaa7e7521c6f0a6d0c2bdc5fde953ad1406e
rwkt21 <rwkt21@bath.ac.uk>
2026-05-16 03:51:39 +0000

    update

--- main.py
+++ main.py
@@ -8,4 +8,24 @@ conn.execute("CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VAR
 
 print ("Table created successfully")
 
-"INSERT INTO pet VALUES (1, 'Fluffy', 'Alice', 'cat', 'f', 5, '2001-02-04', null)"
+conn.execute("INSERT INTO pet (name,owner,species,sex,checkups,birth,death)VALUES \
+  ('Fluffy','Harold','cat','f',5,'2001-02-04','')")
+
+conn.execute("INSERT INTO pet (name,owner,species,sex,checkups,birth,death)VALUES \
+  ('Claws','Gwen','cat','m',2,'2000-03-17','')")
+
+conn.commit()
+print("Records created successfully")
+print("Total number of rows created :", conn.total_changes)
+
+
+cursor = conn.execute("SELECT name,owner,species,sex,checkups,birth,death from pet")
+
+for row in cursor:
+   print("name = ", row[0])
+   print("owner = ", row[1])
+   print("species = ", row[2])
+   print("sex = ", row[3])
+   print("checkups = ", row[4])
+   print("birth = ", row[5])
+   print("death = ", row[6], "\n")
